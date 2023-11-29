e334608e4a2883fce436421d18f2a66671b21e31
Rohit <rowhittank@gmail.com>
2023-11-29 16:45:19 +0530

    1 Assignment **changed dir**

---
+++ MCA271_cdsa/1assignment/cinema.h
@@ -0,0 +1,36 @@
+#ifndef _CINEMA_DB
+#define _CINEMA_DB
+#define MOVIE_SIZE 100
+#define INVALID_CINEMA_ID 0 
+typedef struct Cinema {
+    unsigned int cinema_id;
+    char movie_name[MOVIE_SIZE];
+    unsigned short int release_year;
+    float imdb_rating;
+    unsigned long int box_office_earnings;
+    float runtime;
+}Cinema;
+
+Cinema** create_db(Cinema **, int , int ) ;
+
+float** init_float_arr(float** , int , int ) ;
+
+unsigned long int** init_unsigned_long_int( unsigned long int **, int , int ) ;
+
+void get_movies(Cinema** , int , int ) ;
+
+void display_movies( Cinema** , int , int  ) ;
+
+void disp_imdb_rating( float ** , int , int  ) ;
+
+void disp_box_office_earnings( unsigned long int ** , int , int ) ;
+
+unsigned long int box_office_average(Cinema **, int , int ) ;
+
+void display_movie( Cinema  ) ;
+
+void do_analysis( Cinema** , Cinema** , int , int , int  );
+
+void delete_cinemaDB( Cinema** , int  ) ;
+
+#endif

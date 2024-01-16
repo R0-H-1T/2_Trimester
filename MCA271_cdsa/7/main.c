449eeb9e16679fb66f8dbd6942d49ddf11b7a697
Rohit <rowhittank@gmail.com>
2024-01-16 11:55:49 +0530

    Level order and depth order creation and traversal of tree

--- MCA271_cdsa/7/main.c
+++ MCA271_cdsa/7/main.c
@@ -1,5 +1,44 @@
 #include<stdio.h>
+#include"binary_tree.h"
+#include<stdlib.h>
 
-int main(){
-
+void print_array (int *arr, int n) {
+    for (int i=0; i<n; i++) {
+        printf("%d ", arr[i]);
+    }
 }
+
+
+int main() {
+    int arr[]  = {1,2,3,4,5};
+    Node *root;
+    printf("\nCreating level order tree for arr: "); 
+    print_array(arr, sizeof(arr)/sizeof(int));
+
+    root = createLevelOrderTree (arr, root, 0, sizeof(arr) / sizeof(int));
+    printf("\nLevel Order traversal: ");
+    LevelOrder(root);
+
+
+
+    int inorder[] = {9, 3, 15, 20, 7};
+    int preorder[] = {3, 9, 20, 15, 7};
+    
+    int preindex = 0;
+    int size = sizeof(preorder) / sizeof(int);
+    printf("\n\nCreating binary tree...\n");
+    root = buildTree(preorder, inorder, 0, size-1, &preindex);
+
+    printf("\nTravesal of tree.");
+    printf("\nInorder: ");
+    inorder_traversal(root);
+    printf("\nPreorder: ");
+    preorder_traveral(root);
+
+    free (root);
+
+    return 0;
+}
+
+
+

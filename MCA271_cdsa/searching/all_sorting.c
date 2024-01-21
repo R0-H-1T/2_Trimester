7b8e04ac0d1c47b3f37ed8af2778fa6f624d1487
Rohit <rowhittank@gmail.com>
2024-01-21 17:03:50 +0530

    Merge Sort fixed

--- MCA271_cdsa/searching/all_sorting.c
+++ MCA271_cdsa/searching/all_sorting.c
@@ -71,30 +71,53 @@ void quick_sort (int *arr, int left, int right) {
     }
 }
 
-void merge (int *arr, int left, int mid, int right) {
-    int temp[right - left];
-    int i=0, j=0, count=0;
-    while (i<=mid &&  j<=right) {
-        if (arr[i] <= arr[j]) {
-            temp[count++] = arr[i++];            
-        } else {
-            temp[count++] = arr[j++];
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+ 
+    // Create temp arrays
+    int L[n1], R[n2];
+ 
+    // Copy data to temp arrays L[] and R[]
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+ 
+    // Merge the temp arrays back into arr[l..r
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
         }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
     }
-
-    while (i <= mid) {
-        temp[count++] = arr[i];
+ 
+    // Copy the remaining elements of L[],
+    // if there are any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
     }
-    while (j <= right) {
-        temp[count++] = arr[j];
-    }
-
-
-    for(int i=left, j=0; i<=right; i++, j++) {
-        arr[i] = temp[j];
+ 
+    // Copy the remaining elements of R[],
+    // if there are any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
     }
 }
-
 void merge_sort(int *arr, int left, int right) {
     if (left < right) {
         int mid = (left + right) / 2;
